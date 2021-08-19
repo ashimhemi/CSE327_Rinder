b63203ac6c130d65a8e17ee202e34fe407b72247
Md. Asraful Islam Asif <islam.asif@northsouth.edu>
2021-08-20 00:43:28 +0600

    App folder

---
+++ Rinder/app/src/main/java/com/example/rinder/Home_page.java
@@ -0,0 +1,14 @@
+package com.example.rinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
---
+++ Rinder/app/src/main/java/com/example/rinder/Store.java
@@ -0,0 +1,14 @@
+package com.example.rinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Store extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store);
+    }
+}
