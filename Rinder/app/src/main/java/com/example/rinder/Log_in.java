09a9544bf97993e39c747c4f715d74bbd2852bdf
Md. Asraful Islam Asif <islam.asif@northsouth.edu>
2021-08-23 13:35:57 +0600

    log in add

--- Rinder/app/src/main/java/com/example/rinder/Log_in.java
+++ Rinder/app/src/main/java/com/example/rinder/Log_in.java
@@ -1,14 +1,92 @@
 package com.example.rinder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Log_in extends AppCompatActivity {
+
+    TextView register;
+    EditText email,password;
+    Button login;
+    FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        getSupportActionBar().hide();
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.bc));
+        }//status bar or the time bar at the top
+
+
+        register = findViewById(R.id.register);
+        login = findViewById(R.id.login);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.Password);
+        auth = FirebaseAuth.getInstance();
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Password = password.getText().toString();
+                String Email = email.getText().toString();
+
+                if(Password.isEmpty()){
+                    password.setError("Filed required!");
+                    return;
+                }
+                if(Email.isEmpty()){
+                    email.setError("Filed required!");
+                    return;
+                }
+
+                auth.signInWithEmailAndPassword(Email,Password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Toast.makeText(getApplicationContext(),"Succffully login",Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(),Home_page.class));
+                        finish();
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(),""+e.getMessage(),Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+
+
+
+            }
+        });
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Log_in.this,Registration.class));
+                finish();
+            }
+        });
+
     }
 }
--- Rinder/app/src/main/java/com/example/rinder/Registration.java
+++ Rinder/app/src/main/java/com/example/rinder/Registration.java
@@ -1,14 +1,98 @@
 package com.example.rinder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Registration extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+
+
+
+
+        Button registerRegister;
+        EditText emailRegister,passwordRegister,nameRegister;
+        FirebaseAuth auth;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_registration);
+            registerRegister = findViewById(R.id.registerRegister);
+            nameRegister = findViewById(R.id.nameRegister);
+            emailRegister = findViewById(R.id.emailRegister);
+            passwordRegister = findViewById(R.id.passwordRegister);
+
+            auth = FirebaseAuth.getInstance();
+
+
+            registerRegister.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    String name = nameRegister.getText().toString();
+                    String email = emailRegister.getText().toString();
+                    String password = passwordRegister.getText().toString();
+
+
+
+                    if(name.isEmpty()){
+                        nameRegister.setError("Field must required!");
+                        return;
+                    }
+                    if(email.isEmpty()){
+                        emailRegister.setError("Field must required!");
+                        return;
+                    }
+                    if(password.isEmpty()){
+                        passwordRegister.setError("Field must required!");
+                        return;
+                    }
+                    if(password.length()<6){
+                        passwordRegister.setError("6 digits password required!");
+                        return;
+                    }
+
+                    auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            Toast.makeText(getApplicationContext(),"Registered Successfull !",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),Log_in.class));
+                            finish();
+
+
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(),""+e.getMessage(),Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+
+
+
+
+
+
+
+////                startActivity(new Intent(getApplicationContext(),loginActivity.class));
+//                startActivity(new Intent(getApplicationContext(),afterRegisterActivity.class));
+//                finish();
+                }
+            });
+
+        }
     }
-}
