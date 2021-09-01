3f0d94fcabca03689fce24616bf1e171df7e7ef0
ummay ashim laila <ummay.laila@northsouth.edu>
2021-09-02 01:23:17 +0600

    chat_bot
    from Ual_1812335

---
+++ Rinder/app/src/main/java/com/example/rinder/interfaces/BotReply.java
@@ -0,0 +1,8 @@
+package com.example.rinder.interfaces;
+
+import com.google.cloud.dialogflow.v2.DetectIntentResponse;
+//response detection to chat bot
+public interface BotReply {
+
+    void callback(DetectIntentResponse returnResponse);
+}
