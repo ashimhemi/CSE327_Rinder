df314b8eabf8e35f78fcf4acfc107c5697d27c1f
ummay ashim laila <ummay.laila@northsouth.edu>
2021-09-01 20:58:40 +0600

    shop_activity_added
    mock

---
+++ Rinder/app/src/main/java/com/example/rinder/Users.java
@@ -0,0 +1,15 @@
+package com.example.rinder;
+
+public class Users {
+    public String name,email,gender,subject;
+
+    public Users() {
+    }
+
+    public Users(String name, String email, String gender, String subject) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+        this.subject = subject;
+    }
+}
