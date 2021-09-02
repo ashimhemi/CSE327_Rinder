68e4dee47bdede32685061720640292789772159
Md. Asraful Islam Asif <islam.asif@northsouth.edu>
2021-09-02 21:36:43 +0600

    adding feature done

--- Rinder/app/src/main/java/com/example/rinder/Pview.java
+++ Rinder/app/src/main/java/com/example/rinder/Pview.java
@@ -3,11 +3,16 @@ package com.example.rinder;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.w3c.dom.Text;
 
@@ -20,6 +25,7 @@ public class Pview extends AppCompatActivity {
 
     private TextView textView1,textView2,textView3;
     private Button button;
+    private boolean isPressed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +44,8 @@ public class Pview extends AppCompatActivity {
         textView3=(TextView)findViewById(R.id.subset);
         button=(Button)findViewById(R.id.connectbtn);
 
-
+        //here is my profile uid for current user
+       // final String Uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
         mpostkey=getIntent().getExtras().getString("Package_id");
        // Toast.makeText(getApplicationContext(),mpostkey,Toast.LENGTH_SHORT).show();
@@ -50,5 +57,26 @@ public class Pview extends AppCompatActivity {
         textView1.setText(mname);
         textView2.setText(mgender);
         textView3.setText(msub);
+        button.setOnClickListener(new View.OnClickListener() {
+
+
+
+            @Override
+            public void onClick(View v) {
+                final String rname=mname;
+                final String rgender=mgender;
+                final String rsub =msub;
+                final String key=mpostkey;
+                Request request = new Request(key,rname,rgender,rsub);
+                FirebaseDatabase.getInstance().getReference("request")
+                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(mpostkey).setValue(request);
+
+                Toast.makeText(getApplicationContext(),"Request Sent!",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(),findstudyp.class));
+                finish();
+
+            }
+        });
+
     }
 }
