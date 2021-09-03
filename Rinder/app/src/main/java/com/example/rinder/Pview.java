00518868fd54885be52f1afa40c46d7e6d1474c7
Md. Asraful Islam Asif <islam.asif@northsouth.edu>
2021-09-03 16:41:49 +0600

    Request+Accept done!

--- Rinder/app/src/main/java/com/example/rinder/Pview.java
+++ Rinder/app/src/main/java/com/example/rinder/Pview.java
@@ -1,5 +1,6 @@
 package com.example.rinder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
@@ -12,7 +13,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
@@ -22,9 +28,12 @@ public class Pview extends AppCompatActivity {
     private String mname=null;
     private String mgender=null;
     private String msub=null;
+    private String mmail=null;
 
-    private TextView textView1,textView2,textView3;
+    private TextView textView1,textView2,textView3,textView4;
     private Button button;
+    private DatabaseReference mDatabaseUser;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     private boolean isPressed;
 
     @Override
@@ -42,6 +51,7 @@ public class Pview extends AppCompatActivity {
         textView1=(TextView)findViewById(R.id.namiset);
         textView2=(TextView)findViewById(R.id.genderset);
         textView3=(TextView)findViewById(R.id.subset);
+        textView4=(TextView)findViewById(R.id.mailset);
         button=(Button)findViewById(R.id.connectbtn);
 
         //here is my profile uid for current user
@@ -54,29 +64,62 @@ public class Pview extends AppCompatActivity {
        // Toast.makeText(getApplicationContext(),mname,Toast.LENGTH_SHORT).show();
         mgender=getIntent().getExtras().getString("gender");
         msub=getIntent().getExtras().getString("subject");
+        mmail=getIntent().getExtras().getString("email");
         textView1.setText(mname);
         textView2.setText(mgender);
         textView3.setText(msub);
+        textView4.setText(mmail);
         button.setOnClickListener(new View.OnClickListener() {
 
 
 
             @Override
             public void onClick(View v) {
-                final String rname=mname;
+               /* final String rname=mname;
                 final String rgender=mgender;
                 final String rsub =msub;
+                final String rmail=mmail;
                 final String key=mpostkey;
-                Request request = new Request(key,rname,rgender,rsub);
+                //Request request = new Request(key,rname,rgender,rsub,rmail);
+                Users users = new Users(uid,name,email,gender,subject);
                 FirebaseDatabase.getInstance().getReference("request")
-                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(mpostkey).setValue(request);
+                        .child(mpostkey).setValue(request);
 
                 Toast.makeText(getApplicationContext(),"Request Sent!",Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(getApplicationContext(),findstudyp.class));
                 finish();
+
+                startActivity(new Intent(getApplicationContext(),findstudyp.class));*/
+
+
+                mDatabaseUser= FirebaseDatabase.getInstance().getReference().child("users").child(user.getUid());
+
+                mDatabaseUser.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                        Users userProfileInfo = snapshot.getValue(Users.class);
+                        FirebaseDatabase.getInstance().getReference("request")
+                                .child(mpostkey).child(user.getUid()).setValue(userProfileInfo);
+                        finish();
+
+                        startActivity(new Intent(getApplicationContext(),findstudyp.class));
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
 
             }
         });
 
+    }
+    @Override
+    public void onBackPressed() {
+        Intent intent=new Intent(Pview.this,findstudyp.class);
+        finish();
+        startActivity(intent);
     }
 }
--- Rinder/app/src/main/java/com/example/rinder/RequestList.java
+++ Rinder/app/src/main/java/com/example/rinder/RequestList.java
@@ -1,32 +1,27 @@
 package com.example.rinder;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 
-public class findstudyp extends AppCompatActivity {
-
-    private  RecyclerView recyclerView;
+public class RequestList extends AppCompatActivity {
+    private RecyclerView recyclerView;
 
     private DatabaseReference mDatabaseP;
     private FirebaseAuth mAuth;
@@ -34,53 +29,47 @@ public class findstudyp extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_findstudyp);
+        setContentView(R.layout.activity_request_list);
 
         getSupportActionBar().hide();
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.black2));
         }//status bar or the time bar at the top
 
-        recyclerView=(RecyclerView)findViewById(R.id.recv);
+        recyclerView=(RecyclerView)findViewById(R.id.requestrec);
 
         mAuth=FirebaseAuth.getInstance();
 
-        mDatabaseP = FirebaseDatabase.getInstance().getReference().child("users");
+       // mDatabaseP = FirebaseDatabase.getInstance().getReference().child("users");
 
 
 
         //recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-
     }
-
-
-
 
 
     @Override
     protected void onStart() {
         super.onStart();
-        final String Uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        final String id = FirebaseAuth.getInstance().getCurrentUser().getUid();
         Query query = FirebaseDatabase.getInstance()
                 .getReference()
-                .child("users")
+                .child("request").child(id)
                 .limitToLast(50);
         FirebaseRecyclerOptions<model> options =
                 new FirebaseRecyclerOptions.Builder<model>()
                         .setQuery(query, model.class)
                         .build();
         //Recycler for viewing the information of posts from database
-      //  FirebaseDatabase.getInstance().getReference().child("users").child(Uid);
-
         FirebaseRecyclerAdapter adapter = new FirebaseRecyclerAdapter<model, findstudyp.PackageViewHolder>(options) {
             @Override
             public findstudyp.PackageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
                 // Used same procedure as the posting options for pulling and setting information from database
                 View view = LayoutInflater.from(parent.getContext())
                         .inflate(R.layout.singlerow, parent, false);
-                //FirebaseDatabase.getInstance().getReference().child("users").child(Uid);
+
                 return new findstudyp.PackageViewHolder(view);
             }
 
@@ -92,12 +81,12 @@ public class findstudyp extends AppCompatActivity {
                 holder.setName(model.getName());
                 holder.setGender(model.getGender());
                 holder.setSub(model.getSubject());
-                holder.setEmail(model.getEmail());
 
-/*                if(model.getUid() ==userid ){
 
-                    Uid.setVisibility(View.GONE);
-                }*/
+                if(model.getUid() ==userid ){
+
+                    holder.mView.setVisibility(View.INVISIBLE);
+                }
 
 
 
@@ -108,7 +97,7 @@ public class findstudyp extends AppCompatActivity {
                     public void onClick(View v) {
                         //Toast.makeText(PackageOptions.this,post_key,Toast.LENGTH_SHORT).show();
 
-                        Intent SinglePackageIntent = new Intent(findstudyp.this,Pview.class);
+                        Intent SinglePackageIntent = new Intent(RequestList.this,RequestViewProfile.class);
                         SinglePackageIntent.putExtra("Package_id",post_key);
                         SinglePackageIntent.putExtra("name",model.getName());
                         SinglePackageIntent.putExtra("gender",model.getGender());
@@ -116,7 +105,6 @@ public class findstudyp extends AppCompatActivity {
                         SinglePackageIntent.putExtra("subject",model.getSubject());
                         finish();
                         startActivity(SinglePackageIntent);
-
 
                     }
                 });
@@ -175,13 +163,6 @@ public class findstudyp extends AppCompatActivity {
 
         }
 
-        public void setEmail(String Semail){
-
-            TextView email = (TextView) mView.findViewById(R.id.listmail);
-            email.setText(Semail);
-
-        }
-
 
     }
 
@@ -191,7 +172,5 @@ public class findstudyp extends AppCompatActivity {
         super.onStop();
 
     }
-
-
 
 }
