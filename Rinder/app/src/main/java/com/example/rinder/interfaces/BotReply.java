5c71c10154b7fbe13ab41802c2da7d84f0f0a8e9
Tasnim Alam Piyash <tasnim.piyash@northsouth.edu>
2021-09-05 23:35:57 +0600

    bug

--- Rinder/app/src/main/java/com/example/rinder/interfaces/BotReply.java
+++
@@ -1,9 +0,0 @@
-package com.example.rinder.interfaces;
-
-import com.google.cloud.dialogflow.v2.DetectIntentResponse;
-
-//response detection to chat bot
-public interface BotReply {
-
-    void callback(DetectIntentResponse returnResponse);
-}
